5ec631df485e622bfde5b1e5dc8a60198c405e74
樊启狄 <fqd511@gmail.com>
2019-08-29 14:01:56 +0800

    feat: 增加备案链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -80,7 +80,8 @@ module.exports = {
 
         // git 仓库和编辑链接
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://github.com/fqd511/blog',
+        repo: 'https://www.beian.miit.gov.cn',
+        // repo: 'https://github.com/fqd511/blog',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: 'GitHub',
@@ -96,7 +97,7 @@ module.exports = {
         // 默认是 false, 设置为 true 来启用
         editLinks: true,
         // 默认为 "Edit this page"
-        // editLinkText: '帮助我们改善此页面！'
+        editLinkText: ' 粤ICP备17031034号 '
     },
     locales: {
         // 键名是该语言所属的子路径
